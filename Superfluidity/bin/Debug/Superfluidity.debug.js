dfb3670514529675c5d23894b14df729c205b9d3
Mark Vincze <mrk.vincze@gmail.com>
2011-10-12 19:47:45 +0200

    projekt vaz

---
+++ Superfluidity/bin/Debug/Superfluidity.debug.js
@@ -0,0 +1,18 @@
+//! Superfluidity.debug.js
+//
+
+(function($) {
+
+Type.registerNamespace('Superfluidity');
+
+////////////////////////////////////////////////////////////////////////////////
+// Superfluidity.Class1
+
+Superfluidity.Class1 = function Superfluidity_Class1() {
+}
+
+
+Superfluidity.Class1.registerClass('Superfluidity.Class1');
+})(jQuery);
+
+//! This script was generated using Script# v0.7.3.0
---
+++ Superfluidity/bin/Debug/Superfluidity.js
@@ -0,0 +1,4 @@
+// Superfluidity.js
+(function($){
+Type.registerNamespace('Superfluidity');Superfluidity.Class1=function(){}
+Superfluidity.Class1.registerClass('Superfluidity.Class1');})(jQuery);// This script was generated using Script# v0.7.3.0
