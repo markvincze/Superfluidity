6dd63a909f1152ab6448179c378ea7873df16acb
Daniel Nagy <madve2@gmail.com>
2011-10-14 18:20:31 +0200

    Ezt nagyon fontos volt beletennem

--- Superfluidity/Szuperosztaly.cs
+++ Superfluidity/Szuperosztaly.cs
@@ -5,14 +5,18 @@ using System.Text;
 
 namespace Superfluidity
 {
-    /// <summary>
-    /// TODO: Update summary.
-    /// </summary>
-    public class Szuperosztaly
-    {
-        public void Ezaztanszuperosztaly()
-        {
-            // Ékezetes osztály- és fájlnevet akartam, de a gitnek nem tetszett :(.
-        }
-    }
+	/// <summary>
+	/// TODO: Update summary.
+	/// </summary>
+	public class Szuperosztaly
+	{
+		public int PublikusMezoMertScriptSharpbanNincsPropertySajna;
+
+		public void Ezaztanszuperosztaly()
+		{
+			// Ékezetes osztály- és fájlnevet akartam, de a gitnek nem tetszett :(.
+
+			PublikusMezoMertScriptSharpbanNincsPropertySajna = 42;
+		}
+	}
 }
